ded573431c9f9be3cb3a682226a1f04404b99287
6000699l <141768806+6000699l@users.noreply.github.com>
2023-09-12 15:29:39 -0500

    boton para cambiar tipo de camara

--- c02/threejs_ej02/script.js
+++ c02/threejs_ej02/script.js
@@ -1,10 +1,8 @@
       var scene = new THREE.Scene();
 
       // Configura la cámara
-      //var camera = new THREE.PerspectiveCamera(95, window.innerWidth / window.innerHeight, 0.1, 1000);
-      //camera.position.z = 15;
-
-      var camera = new THREE.OrthographicCamera();
+      var camera = new THREE.PerspectiveCamera(95, window.innerWidth / window.innerHeight, 0.1, 1000);
+      camera.position.z = 15;
       scene.add( camera );
 
       // Configura el renderizador
@@ -18,13 +16,13 @@
       //var materialCube = new THREE.MeshBasicMaterial({ map: texture });
       
       var geometryCube = new THREE.BoxGeometry();
-      var materialCube = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      var materialCube = new THREE.MeshNormalMaterial({ color: 0x00ff00 });
       var cube = new THREE.Mesh(geometryCube, materialCube);
       scene.add(cube);
 
       // Crea una esfera
       var geometrySphere = new THREE.SphereGeometry(1,30,30);
-      var materialSphere = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+      var materialSphere = new THREE.MeshNormalMaterial({ color: 0xff0000 });
       var sphere = new THREE.Mesh(geometrySphere, materialSphere);
       sphere.position.x = -2;
       scene.add(sphere);
@@ -45,3 +43,29 @@
      
 
       animate();
+
+       // Función para cambiar a cámara ortográfica
+       function changeToOrthographic() {
+        camera = new THREE.OrthographicCamera(-2,fov2Slider.value, 2,-2, 0.0001, 1000);
+        camera.position.z =5;
+        camera.updateProjectionMatrix();
+        
+      }
+
+      function changeToPerspective() {
+        camera = new THREE.PerspectiveCamera(fovSlider.value, window.innerWidth / window.innerHeight, 0.1, 1000);
+        camera.position.z = 5;
+        camera.updateProjectionMatrix();
+      }
+
+      // Escucha los cambios en el deslizador y actualiza la cámara
+      var fovSlider = document.getElementById('fovSlider');
+      fovSlider.addEventListener('input', function () {
+        changeToPerspective();
+      });
+
+      var fov2Slider = document.getElementById('fov2Slider');
+      fov2Slider.addEventListener('input', function () {
+        changeToOrthographic();
+      });
+
