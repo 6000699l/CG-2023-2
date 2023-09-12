407a4c50b0061578f4c0e61312e03e5466f9bcf7
6000699l <141768806+6000699l@users.noreply.github.com>
2023-09-12 15:47:40 -0500

    funciones a la camara en prespectiva
    agrege deslizadores que controlan la posicion en x,y,z de la camara

--- c02/threejs_ej02/script.js
+++ c02/threejs_ej02/script.js
@@ -2,7 +2,7 @@
 
       // Configura la cámara
       var camera = new THREE.PerspectiveCamera(95, window.innerWidth / window.innerHeight, 0.1, 1000);
-      camera.position.z = 15;
+      camera.position.z = 25;
       scene.add( camera );
 
       // Configura el renderizador
@@ -28,34 +28,38 @@
       scene.add(sphere);
 
       // Renderiza la escena
-      var animate = function () {
-        requestAnimationFrame(animate);
+     // var animate = function () {
+       //requestAnimationFrame(animate);
 
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+        //cube.rotation.x += 0.01;
+       // cube.rotation.y += 0.01;
 
-        sphere.rotation.x += 0.01;
-        sphere.rotation.y += 0.01;
+       // sphere.rotation.x += 0.01;
+      //  sphere.rotation.y += 0.01;
 
-        renderer.render(scene, camera);
-      };
+        
+     // };
 
      
 
-      animate();
+      //animate();
 
        // Función para cambiar a cámara ortográfica
        function changeToOrthographic() {
-        camera = new THREE.OrthographicCamera(-2,fov2Slider.value, 2,-2, 0.0001, 1000);
+        camera = new THREE.OrthographicCamera(-2,2, 2,-2, 0.0001, 1000);
         camera.position.z =5;
         camera.updateProjectionMatrix();
+        renderer.render(scene, camera);
         
       }
 
       function changeToPerspective() {
         camera = new THREE.PerspectiveCamera(fovSlider.value, window.innerWidth / window.innerHeight, 0.1, 1000);
-        camera.position.z = 5;
+        camera.position.z = fov2Slider.value;
+        camera.position.x = fov3Slider.value;
+        camera.position.y = fov4Slider.value;
         camera.updateProjectionMatrix();
+        renderer.render(scene, camera);
       }
 
       // Escucha los cambios en el deslizador y actualiza la cámara
@@ -66,6 +70,14 @@
 
       var fov2Slider = document.getElementById('fov2Slider');
       fov2Slider.addEventListener('input', function () {
-        changeToOrthographic();
+        changeToPerspective();
+      });
+      var fov3Slider = document.getElementById('fov3Slider');
+      fov3Slider.addEventListener('input', function () {
+        changeToPerspective();
+      });
+      var fov4Slider = document.getElementById('fov4Slider');
+      fov4Slider.addEventListener('input', function () {
+        changeToPerspective();
       });
 
