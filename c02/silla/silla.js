80c5e9a3b054220e6c0c71e08c0f0b7d834de135
6000699l <141768806+6000699l@users.noreply.github.com>
2023-09-19 14:12:48 -0500

    Add files via upload

--- c02/silla/silla.js
+++ c02/silla/silla.js
@@ -1,8 +1,8 @@
  // Configurar la escena
  const scene = new THREE.Scene();
  const camera = new THREE.PerspectiveCamera(95, window.innerWidth / window.innerHeight, 0.1, 1000);
- camera.position.x = 2;
- camera.position.y = 2;
+ camera.position.y = 1;
+ //camera.position.y = 2;
  scene.add( camera );
  const renderer = new THREE.WebGLRenderer();
  renderer.setSize(window.innerWidth, window.innerHeight);
@@ -13,9 +13,21 @@
  const material = new THREE.MeshNormalMaterial(); // Color verde
  const cube = new THREE.Mesh(geometry, material);
  const cube1 = new THREE.Mesh(geometry, material);
- cube1.position.x = -2;
- cube.scale.set(0.05, 0.5, 0.05);
- scene.add(cube,cube1);
+ const cube2 = new THREE.Mesh(geometry, material);
+ const cube3 = new THREE.Mesh(geometry, material);
+ const cube4 = new THREE.Mesh(geometry, material);
+ const cube5 = new THREE.Mesh(geometry, material);
+ cube.scale.set(0.4, 0.1, 0.4);
+ cube1.scale.set(0.05, 0.5, 0.05);
+ cube2.scale.set(0.05, 0.5, 0.05);
+ cube3.scale.set(0.05, 0.5, 0.05);
+ cube4.scale.set(0.05, 0.5, 0.05);
+ cube.position.y = 0.1;
+ cube1.position.set(-0.4,-0.5,-0.4);
+ cube2.position.set(0.4,-0.5,-0.4);
+ cube3.position.set(-0.4,-0.5,0.4);
+ cube4.position.set(0.4,-0.5,0.4);
+ scene.add(cube,cube1,cube2,cube3,cube4);
 
  // Posicionar la cámara
  camera.position.z = 5;
